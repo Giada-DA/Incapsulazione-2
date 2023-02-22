a5957335c07b1e2ceefc164d1bae45414873cdd9
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-22 22:11:59 +0100

    Esercizio 2 sull'incapsulazione

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -11,16 +11,17 @@ public class Start {
         System.out.println("Inserisci cognome: ");
         String surname = scanner.nextLine();
         System.out.println("Inserisci altezza (es. 1,60): ");
-        double height = scanner.nextDouble();
+        double height = Math.floor(scanner.nextDouble()*100.0)/100.0;
         System.out.println("Inserisci età: ");
         int age = scanner.nextInt();
+
         Person persona = new Person();
         persona.setName(name);
         persona.setSurname(surname);
         persona.setHeight(height);
         persona.setAge(age);
 
-        System.out.println("Nome: " + persona.getName() + ", cognome: " + persona.getSurname() + ", altezza: " + persona.getHeight() + " e età: " + persona.getAge());
+        System.out.println("Nome: " + persona.getName() + ", cognome: " + persona.getSurname() + ", altezza: " + height + " e età: " + persona.getAge());
         //richiamando il metodo toString
         System.out.println(persona.toString());
     }
